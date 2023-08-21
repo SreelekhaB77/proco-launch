26e36a4f20fb5448fc2885025c248ce36c103dff
kavithadayyala <kavitha.dayyala2@mindtree.com>
2023-08-21 13:08:30 +0530

    Additinal quantity primary and customer closing reqular quantity changes

--- src/main/java/com/hul/proco/controller/createpromo/DataFromTable.java
+++ src/main/java/com/hul/proco/controller/createpromo/DataFromTable.java
@@ -52,8 +52,8 @@ public class DataFromTable {
 					+ "INNER JOIN  (SELECT CHANNEL_NAME,BASEPACK,SALES_CATEGORY FROM (SELECT ROW_NUMBER() OVER (PARTITION BY BASEPACK, CHANNEL_NAME ORDER BY BASEPACK,SALES_CATEGORY) AS ROW_NUM,CHANNEL_NAME,BASEPACK,SALES_CATEGORY FROM TBL_PROCO_PRODUCT_MASTER_V2 A WHERE IS_ACTIVE=1 GROUP BY CHANNEL_NAME,BASEPACK,SALES_CATEGORY) A WHERE ROW_NUM=1) C ON C.BASEPACK=T.BASEPACK_CODE AND C.CHANNEL_NAME = T.CHANNEL_NAME "
 					+ "INNER JOIN TBL_PROCO_SOL_TYPE S ON S.SOL_REMARK=T.CR_SOL_TYPE "
 					+ " " + "SET "
-					+ "PPM_DESC_STAGE=CONCAT(A.ACCOUNT_TYPE ,':',  B.MODALITY_KEY,':',T.PPM_ACCOUNT,':',C.SALES_CATEGORY,':',S.SOL_TYPE,':',T.OFFER_DESC,':',T.PROMOTION_ID,IF (((A.NON_UNIFY <> '') OR (A.ACCOUNT_TYPE = 'KA' AND A.NON_UNIFY <> 'NON UNIFY' AND T.CR_SOL_TYPE = 'Additional Quantity')), concat(':', 'NON UNIFY'), ''))  WHERE USER_ID='"
-					+ uid + "'  " + " AND T.MOC IN( "+mocList+")"; //moc added by kavitha D-Sprint 14 changes
+					+ "PPM_DESC_STAGE=CONCAT(A.ACCOUNT_TYPE ,':',  B.MODALITY_KEY,':',T.PPM_ACCOUNT,':',C.SALES_CATEGORY,':',S.SOL_TYPE,':',T.OFFER_DESC,':',T.PROMOTION_ID,IF (((A.NON_UNIFY <> '') OR (A.ACCOUNT_TYPE = 'KA' AND A.NON_UNIFY <> 'NON UNIFY' AND T.CR_SOL_TYPE IN('Additional Quantity- Primary','Additional Quantity- Customer Closing')), concat(':', 'NON UNIFY'), ''))  WHERE USER_ID='"
+					+ uid + "'  " + " AND T.MOC IN( "+mocList+")"; //moc added by kavitha D-Sprint 14 changes //replaced additional quantity with new additional qunatity sol types-SPRINT 17
 		} else {
 			
 			update_ppm_desc = "UPDATE TBL_PROCO_PROMOTION_MASTER_TEMP_V2 T "
